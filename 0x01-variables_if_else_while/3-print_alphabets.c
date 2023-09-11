08c0cfdcbbe5d64c9a115fc174b6ffb1012b171b
Kipkiruie <elishak551@gmail.com>
2023-09-11 11:55:13 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ */
+int main(void)
+{	char low;
+	for (low = 'a'; low <= 'z'; low++)
+	putchar ('\n');
+	return (0);
+}
