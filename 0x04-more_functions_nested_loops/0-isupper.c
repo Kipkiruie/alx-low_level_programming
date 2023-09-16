cd84ed1e479cd59dd28f7b2f3d17bfa9184f9bcc
Kipkiruie <elishak551@gmail.com>
2023-09-15 19:51:24 -0700

    isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper -checks uppercase
+ * c: variable text
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'z')
+{return (1);
+}
+else
+{
+return (0);
+}
+}
