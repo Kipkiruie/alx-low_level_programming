dc6e18fde0d1aae8ca24d64bd3398a63ee6c976b
Kipkiruie <elishak551@gmail.com>
2023-09-17 20:03:24 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include"main.h"
+
+/**
+ * print_square - print a square using the character #
+@@ -10,23 +10,12 @@
+
+void print_square(int size)
+{
+	int c, r;
+	int row, column;
+
+	if (!(size <= 0))
+	{
+		for (c = 0; c < size; c++)
+		{
+			r = 0;
+			while (r < size)
+			{
+				_putchar('#');
+				r++;
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	for (row = 1; row <= size; ++row)
+	{
+		for (column = 1; column <= size; ++column)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
