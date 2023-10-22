b0291292e62cddb9dd222333e6ee606050bfc6d4
Kipkiruie <elishak551@gmail.com>
2023-10-22 06:01:59 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,14 @@
-#include "holberton.h"
 #include"main.h"
 
 /**
  * print_numbers - print 0 - 9
-@@ -9,10 +9,11 @@
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
 
 void print_numbers(void)
 {
-	int i;
-
-	for (i = 0; i <= 9; i++)
-		_putchar(i + 48);
 	int num = 0;
 
 	do {
