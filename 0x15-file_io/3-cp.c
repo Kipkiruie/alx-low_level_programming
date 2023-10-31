bf7890f6c1afcbd2811f59015d980595af706f6e
Kipkiruie <elishak551@gmail.com>
2023-10-31 09:07:51 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,88 +1,53 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-#define MAXSIZE 1024
-
-
-/**
- * __exit - prints error messages and exits with exit number
- *
- * @error: either the exit number or file descriptor
- * @str: name of either file_in or file_out
- * @fd: file descriptor
- *
- * Return: 0 on success
-*/
-int __exit(int error, char *str, int fd)
+void error_message(int code, const char *format, ...)
 {
-	switch (error)
-	{
-		case 97:
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			exit(error);
-		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-			exit(error);
-		case 99:
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-			exit(error);
-		case 100:
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-			exit(error);
-		default:
-			return (0);
-	}
+	va_list args;
+
+	va_start(args, format);
+	dprintf(STDERR_FILENO, format, args);
+	va_end(args);
+	exit(code);
 }
 
 /**
- * main - create a copy of file
- *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: 0 for success.
-*/
+ *main - checks code
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-	int file_in, file_out;
-	int read_stat, write_stat;
-	int close_in, close_out;
-	char buffer[MAXSIZE];
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUF_SIZE];
 
-	/*if arguments are not 3*/
 	if (argc != 3)
-		__exit(97, NULL, 0);
+		error_message(97, "Usage: cp file_from file_to\n");
 
-	/*sets file descriptor for copy from file*/
-	file_in = open(argv[1], O_RDONLY);
-	if (file_in == -1)
-		__exit(98, argv[1], 0);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_message(97, "Usage: cp file_from file_to\n");
 
-	/*sets file descriptor for copy to file*/
-	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_out == -1)
-		__exit(99, argv[2], 0);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+	if (fd_to == -1)
+		error_message(99, "Error: Can't write to file %s\n", argv[2]);
 
-	/*reads file_in as long as its not NULL*/
-	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
+	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
-		if (read_stat == -1)
-			__exit(98, argv[1], 0);
-
-		/*copy and write contents to file_out*/
-		write_stat = write(file_out, buffer, read_stat);
-		if (write_stat == -1)
-			__exit(99, argv[2], 0);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+			error_message(99, "Error: Can't write to file %s\n", argv[2]);
 	}
 
-	close_in = close(file_in); /*close file_in*/
-	if (close_in == -1)
-		__exit(100, NULL, file_in);
+	if (bytes_read == -1)
+		error_message(98, "Error: Can't read from file %s\n", argv[1]);
 
-	close_out = close(file_out); /*close file_out*/
-	if (close_out == -1)
-		__exit(100, NULL, file_out);
+	if (close(fd_from) == -1)
+		error_message(100, "Error: Can't close fd %d\n", fd_from);
+
+	if (close(fd_to) == -1)
+		error_message(100, "Error: Can't close fd %d\n", fd_to);
 
 	return (0);
 }
