fed43782fdbc47777554a2c6624f86201f77a839
Kipkiruie <elishak551@gmail.com>
2023-10-31 09:44:40 -0700

    main header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,24 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUF_SIZE 1024
 
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+size_t _strlen(const char *s);
+void print_abi(unsigned char *e_ident);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+
+#endif /* MAIN_H */
